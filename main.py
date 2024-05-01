b77ea874564d9b2daadcaad429b2bd518aa837b0
aurshine <2901658788@qq.com>
2024-05-02 00:32:06 +0800

    main

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from ark.analyse import analyse
+from ark.analyse import analyse, ByType
 
-print(analyse('你就是歌姬吧，你记住你什么都不是'))
+comments = ['你就是歌姬吧，你记住你什么都不是',
+            '杀马特团长你等着我，我和你没完，你等着！！',
+            '小亮给他整个活',
+            'python是世界上最好的语言',
+            '鸡你太美',
+            '原神启动']
+print(analyse(comments, by=ByType.BY_TEXT))
